990c675b9702a7700b4e721bd735ad3ed9f8d34e
Itamar Juwiler <itamarj@campus.technion.ac.il>
2022-05-02 18:02:49 +0300

    Pushed camera and engine code

---
+++ engine/engine.ino
@@ -0,0 +1,45 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+// Motor A
+int motor1Pin1 = 14; 
+int motor1Pin2 = 15; 
+
+void setup() {
+  Serial.begin(115200);
+
+  // testing
+  Serial.print("Testing DC Motor...");
+  
+  // sets the pins as outputs:
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+}
+
+void loop() {
+  // Move the DC motor forward at maximum speed
+  Serial.println("Moving Forward");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH); 
+  delay(5000);
+
+  // Stop the DC motor
+  Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  delay(1000);
+
+  // Move DC motor backwards at maximum speed
+  Serial.println("Moving Backwards");
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW); 
+  delay(5000);
+
+  // Stop the DC motor
+  Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  delay(1000);
+}
